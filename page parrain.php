d41cf5d908b688a1052aebf4f0e17d825e8fef60
Nifath <138664107+Nifath@users.noreply.github.com>
2023-08-03 10:51:11 +0200

    page parrain.php

--- page parrain.php
+++ page parrain.php
@@ -18,7 +18,7 @@ if (isset($_POST['submit'])) {
     $prenom = $_POST['prenom'];
 
     // Exemple de requête SQL pour récupérer les informations des enfants parrainés par le parrain
-    $query = "SELECT * FROM dossier1 WHERE nom = :nom AND prenom = :prenom";
+    $query = "SELECT * FROM dossier WHERE nom = :nom AND prenom = :prenom";
     $stmt = $pdo->prepare($query);
     $stmt->execute(['nom' => $nom, 'prenom' => $prenom]);
     $enfantsParraines = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -32,47 +32,33 @@ if (isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
-    <title>Recherche des critères des enfants parrainés</title>
+    <title>Recherche des enfants parrainés</title>
     <style>
         body {
-  background-image: url("image0.jpg");
-  background-size: cover;
-  background-position: center;
-}
-
-        h1 {
-            text-align: center;
-            margin-bottom: 30px;
+            background-image: url("image0.jpg");
+            background-size: cover;
+            background-position: center;
         }
 
-        form {
+        .container {
             max-width: 400px;
             margin: 0 auto;
             background-color: #fff;
             padding: 20px;
             border-radius: 5px;
             box-shadow: 0px 0px 5px 1px #ddd;
+            margin-top: 50px;
         }
 
-        label {
-            font-weight: bold;
+        .form-group {
+            display: flex;
+            flex-direction: column;
         }
-        button{
-            background-color: #007bff;
-            color: #fff;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-        input[type="number"],
-        input[type="text"],
-        select {
-            width: 100%;
-            padding: 8px;
-            margin-bottom: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
+
+        h1 {
+            text-align: center;
+            margin-bottom: 30px;
+            color: #007bff;
         }
 
         button[type="submit"] {
@@ -80,38 +66,43 @@ if (isset($_POST['submit'])) {
             color: #fff;
             padding: 10px 20px;
             border: none;
-            border-radius: 5px;
+            border-radius: 2px;
             cursor: pointer;
+            width: 100%;
         }
 
         button[type="submit"]:hover {
             background-color: #0056b3;
         }
+
+        .results {
+            margin-top: 20px;
+        }
     </style>
+
 </head>
 <body>
-<header class="topbar">
-        <div class="col-md-12">
-         <div class="logo">
-                <img src="logo.png" alt="Venue Logo" style="width: 120px;">
-            </div>
-    </header>
-    <form method="POST" action="#">
-        <div class="form-group">
-        <label for="nom">Nom:</label>
-            <input type="text" id="nom" name="nom" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="prenom">Prenom(s):</label>
-            <input type="text" id="prenom" name="prenom" class="form-control">
-        </div>
-       
-        <button type="submit" class="btn btn-primary">Rechercher</button>
-       
-    </form>
-   
-    <?php if (!empty($enfantsParraines)): ?>
-            <h2>Résultats de la Recherche :</h2>
+<div class="container">
+        <div class="row justify-content-center"> 
+             
+                <form method="POST" action="#">
+                    <div class="form-row"> 
+                        <div class="form-group col-md-12">
+                            <label for="nom">Nom:</label>
+                            <input type="text" id="nom" name="nom" class="form-control" required>
+                        </div>
+                        <div class="form-group col-md-12">
+                            <label for="prenom">Prénom(s):</label>
+                            <input type="text" id="prenom" name="prenom" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" name="submit" class="btn btn-primary">Rechercher</button>
+                    </div>
+                </form>
+
+        <?php if (!empty($enfantsParraines)): ?>
+            <h2 class="mt-4">Résultats de la Recherche :</h2>
             <ul>
                 <?php foreach ($enfantsParraines as $enfant): ?>
                 <li>
@@ -127,13 +118,12 @@ if (isset($_POST['submit'])) {
                     Etablissement:<?php echo $enfant['etablissement']; ?><br>
                     Date de parrainage: <?php echo $enfant['dateParrainage']; ?><br>
                     Renseignement:<?php echo $enfant['renseignement']; ?><br>
-
-                    <!-- Ajouter d'autres informations spécifiques sur l'enfant -->
                 </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php else: ?>
-        <p>Aucun enfant trouvé avec ces nom et prénom.</p>
-    <?php endif; ?>
+                <?php endforeach; ?>
+            </ul>
+        <?php elseif (isset($_POST['submit'])): ?>
+            <p>Aucun enfant trouvé avec ces nom et prénom.</p>
+        <?php endif; ?>
+    </div>
 </body>
 </html>
