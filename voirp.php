a2500e6015bd3891f85892aafeb30e2f0f45fd0a
Nifath <138664107+Nifath@users.noreply.github.com>
2023-08-10 12:33:53 +0200

    voirp.php

--- voirp.php
+++ voirp.php
@@ -1,13 +1,5 @@
+
 <?php
-// voir.php
-// Récupérer l'ID de l'enfant à afficher depuis l'URL (passé en paramètre)
-$id_dossier1 = $_GET['id_dossier1'];
-
-// Vérifier si l'ID de l'enfant est valide
-if (!is_numeric($id_dossier1)) {
-    die("ID de l'enfant invalide.");
-}
-
 // Connexion à la base de données
 $conn = new mysqli('localhost', 'root', '', 'caeb');
 
@@ -16,28 +8,162 @@ if ($conn->connect_error) {
     die("La connexion à la base de données a échoué: " . $conn->connect_error);
 }
 
-// Récupérer les informations de l'enfant à partir de la base de données en fonction de l'ID
-$instruct = $conn->prepare('SELECT * FROM dossier1 WHERE id_dossier1 = ?');
-$instruct->bind_param('i', $id_enfant);
+// Récupérer l'ID du dossier depuis le paramètre d'URL
+$id_dossier = $_GET['id'];
+
+// Récupérer les informations du dossier depuis la base de données
+$instruct = $conn->prepare('SELECT * FROM dossier WHERE id_dossier = ?');
+$instruct->bind_param('i', $id_dossier);
 $instruct->execute();
-$result = $instruct->get_result();
-$row = $result->fetch_assoc();
+$result = $instruct->get_result()->fetch_assoc();
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Détails de l'enfant</title>
+    <title>Voir le profil</title>
+
+    <style>
+     body {
+            font-family: Arial, sans-serif;
+            background-image: url("image16.jpg");
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            border-radius: 6px;
+        }
+
+        .profile-header {
+            display: flex;
+            align-items: center;
+            padding: 20px;
+            background-color: #3498db;
+            color: white;
+        }
+
+        .profile-image {
+            width: 60px;
+            height: 60px;
+            border-radius: 50%;
+            margin-right: 20px;
+        }
+
+       
+
+        .profile-name {
+            font-size: 24px;
+            font-weight: bold;
+            margin: 0;
+        }
+
+        .profile-container {
+           display: flex;
+            align-items: flex-start; 
+            margin-bottom: 20px;
+}
+
+        .profile-image {
+            width: 60px;
+            height: 60px;
+            border-radius: 50%;
+            margin-right: 20px;
+        }
+
+        .profile-info {
+            flex: 1;
+        }
+
+        .profile-details {
+            flex: 1;
+            padding-left: 20px; 
+            border-left: 1px solid #ccc; 
+        }
+
+        .return-link {
+            width: fit-content;
+    margin-bottom:20px;
+    background-color:#3498db;
+    padding: 5px 20px;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-align: 0;
+    border-radius: 6px;
+    text-decoration: 0; 
+        }
+
+        button{
+    width: fit-content;
+    margin-bottom:20px;
+    background-color:#3498db;
+    padding: 5px 20px;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-align: 0;
+    border-radius: 6px;
+    text-decoration: 0; 
+}
+
+.profile-details p {
+    margin: 20px 0; 
+}
+
+    </style>
+
+    <script src="html2pdf.bundle.js"></script>
 </head>
 <body>
-    <h1>Détails de l'enfant</h1>
-    <p><strong>Nom :</strong> <?= $row['nom'] ?></p>
-    <p><strong>Prénom(s) :</strong> <?= $row['prenom'] ?></p>
-    <p><strong>Sexe :</strong> <?= $row['sexe'] ?></p>
-    <p><strong>Date de Naissance :</strong> <?= $row['dateNaissance'] ?></p>
-    <p><strong>Classe :</strong> <?= $row['classe'] ?></p>
-    <!-- Afficher d'autres informations de l'enfant ici -->
+<header> <button onclick="goToAnotherPage()">Retour</button></header>
+    <div class="container">
+        <div class="profile-header">
+            <img class="profile-image" src="data:image/jpeg;base64,<?= base64_encode($result['photoProfil']) ?>" alt="Photo de profil">
+            <div class="profile-info">
+                <h1 class="profile-name"><?= $result['nom'] ?> <?= $result['prenom'] ?></h1>
+            </div>
+        </div>
+        <div class="profile-details">
+            <?php if (!empty($result['photoProfil'])): ?>
+                <img src="data:image/jpeg;base64,<?= base64_encode($result['photoProfil']) ?>" alt="Photo de profil" style="max-width: 100px; border-radius: 6px;">
+            <?php else: ?>
+                <p>Aucune photo de profil disponible.</p>
+            <?php endif; ?>
+            <p><strong>Nom:</strong> <?= $result['nom'] ?></p>
+                    <p><strong>Prénom(s):</strong> <?= $result['prenom'] ?></p>
+                    <p><strong>Date de Naissance:</strong> <?= $result['dateNaissance'] ?></p> 
+                    <p><strong>Sexe:</strong> <?= $result['sexe'] ?></p>
+                    <p><strong>Lieu de Naissance:</strong> <?= $result['lieuNaissance'] ?></p>
+                    <p><strong>Nom du père:</strong> <?= $result['nomPere'] ?></p>
+                    <p><strong>Nom de la mère:</strong> <?= $result['nomMere'] ?></p>
+                    <p><strong>Nom du tuteur:</strong> <?= $result['nomTuteur'] ?></p>
+                    <p><strong>Classe:</strong> <?= $result['classe'] ?></p>
+                    <p><strong>Établissement:</strong> <?= $result['etablissement'] ?></p>
+                    <p><strong>Renseignement justifiant le parrainage:</strong> <?= $result['renseignement'] ?></p>
+        </div>
+        
+    </div>
+    <center><a class="return-link" onclick="generatePDF()">Télécharger</a></center>
+    <script>
+
+function goToAnotherPage() {   
+        window.location.href = "listep.php"; 
+    } 
+        function generatePDF() {
+            var element = document.querySelector(".container");
+            var fileName = encodeURIComponent("<?= $result['nom'] ?><?= $result['prenom'] ?>");
+    html2pdf().from(element).save(fileName);
+        }
+
+        
+    </script>
 </body>
 </html>
