b1e3b560d6e0f3146e54a56482ba38dea247e132
Nifath <138664107+Nifath@users.noreply.github.com>
2023-08-10 12:24:12 +0200

    criteresp.php

--- criteresp.php
+++ criteresp.php
@@ -48,7 +48,8 @@
             border-radius: 5px;
         }
 
-        button[type="submit"] {
+        button[type="submit"],
+        button[type="reset"] {
             background-color: #007bff;
             color: #fff;
             padding: 10px 20px;
@@ -57,7 +58,8 @@
             cursor: pointer;
         }
 
-        button[type="submit"]:hover {
+        button[type="submit"],
+        button[type="reset"]:hover {
             background-color: #0056b3;
         }
     </style>
@@ -90,10 +92,15 @@
             <input type="text" id="classe" name="classe" class="form-control">
         </div>
         <button type="submit" class="btn btn-primary">Rechercher</button>
+        <button type="reset" class="btn btn-primary" onclick="goToAnotherPage()">Retour</button>
        
     </form>
    
-
+    <script>
+    function goToAnotherPage() {   
+        window.location.href = "profil.php"; 
+    }  
+</script> 
 
 </body>
 </html>
